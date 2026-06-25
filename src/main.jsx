6cefb9366c6be62128425e49daf32a01c9a1d1cf
sny9brian7 <syogoyusyu.14@gmail.com>
2026-06-25 17:45:27 +0900

    Add Vercel Analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import { ThemeProvider } from './theme/ThemeContext.jsx'
 import './index.css'
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>,
 )
